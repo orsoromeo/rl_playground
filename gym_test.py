7515de4922b8d531d31756956938eadd425e168d
Romeo Orsolino <rome_91@hotmail.it>
2023-03-12 16:35:07 +0000

    first commit

---
+++ gym_test.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+env = gym.make("CartPole-v1")
+print("finished")
