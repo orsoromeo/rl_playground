c6c4617a3925f01035164a58a57e899d93a15bd6
Romeo Orsolino <romeo.orsolino@ocado.com>
2025-03-04 10:56:38 +0000

    rename folders

--- src/rl_specialization/2 - Sample based RL/assignment3/assignment3_TD0.py
+++ src/rl_specialization/2 - Sample based RL/assignment3/assignment3_TD0.py
@@ -154,7 +154,7 @@ def run_experiment(env_info, agent_info,num_episodes=5000, experiment_name=None,
     rl_glue.rl_init(agent_info, env_info)
 
     manager = Manager(env_info, agent_info, true_values_file=true_values_file, experiment_name=experiment_name)
-    for episode in range(1, num_episodes + 1):
+    for episode in tqdm(range(1, num_episodes + 1)):
         rl_glue.rl_episode(0) # no step limit
         if episode % plot_freq == 0:
             values = rl_glue.agent.agent_message("get_values")
@@ -168,13 +168,13 @@ agent_info = {"discount": 1, "step_size": 0.01, "seed": 0}
 
 # The Optimal Policy that strides just along the cliff
 policy = np.ones(shape=(env_info['grid_width'] * env_info['grid_height'], 4)) * 0.25
-# policy[36] = [1, 0, 0, 0]
-# for i in range(24, 35):
-#     policy[i] = [0, 0, 0, 1]
-# policy[35] = [0, 0, 1, 0]
+policy[36] = [1, 0, 0, 0]
+for i in range(24, 35):
+    policy[i] = [0, 0, 0, 1]
+policy[35] = [0, 0, 1, 0]
 
 agent_info.update({"policy": policy})
 
 true_values_file = "optimal_policy_value_fn.npy"
-_ = run_experiment(env_info, agent_info, num_episodes=5000, experiment_name="Policy Evaluation on Optimal Policy",
-                   plot_freq=500, true_values_file=true_values_file)
+_ = run_experiment(env_info, agent_info, num_episodes=500, experiment_name="Policy Evaluation on Optimal Policy",
+                   plot_freq=50, true_values_file=true_values_file)
