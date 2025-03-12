682771ed7c3fbbcd905f3774b02b183133277a22
Romeo Orsolino <romeo.orsolino@ocado.com>
2025-03-12 17:00:17 +0000

    dqn with 3 layers no pytorch

--- src/rl_specialization/tests/LunarLandingWithDQN/lunar_landing_w_dqn.py
+++ src/rl_specialization/tests/LunarLandingWithDQN/lunar_landing_w_dqn.py
@@ -76,7 +76,7 @@ class Agent():
         """
         self.replay_buffer = ReplayBuffer(agent_config['replay_buffer_size'], 
                                           agent_config['minibatch_sz'], agent_config.get("seed"))
-        self.network = ActionValueNetwork(agent_config['network_config'])
+        self.network = ActionValueNetwork2L(agent_config['network_config'])
         self.optimizer = Adam(self.network.layer_sizes, agent_config["optimizer_config"])
         self.num_actions = agent_config['network_config']['num_actions']
         self.num_replay = agent_config['num_replay_updates_per_step']
@@ -101,8 +101,8 @@ class Agent():
             the action. 
         """
         action_values = self.network.get_action_values(state)
-        tau = 1 * (500 - self.episode) / 500
-        probs_batch = softmax(action_values, tau)
+        # tau = 1 * (500 - self.episode) / 500
+        probs_batch = softmax(action_values, self.tau)
         action = self.rand_generator.choice(self.num_actions, p=probs_batch.squeeze())
         return action
 
@@ -460,8 +460,131 @@ class Adam():
         
         return weights
 
+class ActionValueNetwork2L:
+    # Work Required: Yes. Fill in the layer_sizes member variable (~1 Line).
+    def __init__(self, network_config):
+        self.state_dim = network_config.get("state_dim")
+        self.num_hidden_units = network_config.get("num_hidden_units")
+        self.num_actions = network_config.get("num_actions")
+        
+        self.rand_generator = np.random.RandomState(network_config.get("seed"))
+        
+        # Specify self.layer_sizes which shows the number of nodes in each layer
+        # your code here
+        self.layer_sizes = [self.state_dim, self.num_hidden_units, self.num_hidden_units, self.num_actions]
+        
+        # Initialize the weights of the neural network
+        # self.weights is an array of dictionaries with each dictionary corresponding to 
+        # the weights from one layer to the next. Each dictionary includes W and b
+        self.weights = [dict() for i in range(0, len(self.layer_sizes) - 1)]
+        for i in range(0, len(self.layer_sizes) - 1):
+            self.weights[i]['W'] = self.init_saxe(self.layer_sizes[i], self.layer_sizes[i + 1])
+            self.weights[i]['b'] = np.zeros((1, self.layer_sizes[i + 1]))
+    
+    # Work Required: No.
+    def get_action_values(self, s):
+        """
+        Args:
+            s (Numpy array): The state.
+        Returns:
+            The action-values (Numpy array) calculated using the network's weights.
+        """
+        
+        W0, b0 = self.weights[0]['W'], self.weights[0]['b']
+        psi0 = np.dot(s, W0) + b0
+        x0 = np.maximum(psi0, 0)
+        
+        W1, b1 = self.weights[1]['W'], self.weights[1]['b']
+        psi1 = np.dot(x0, W1) + b1
+        x1 = np.maximum(psi1, 0)
 
-class ActionValueNetwork:
+        W2, b2 = self.weights[2]['W'], self.weights[2]['b']
+        q_vals = np.dot(x1, W2) + b2
+
+        return q_vals
+    
+    # Work Required: No.
+    def get_TD_update(self, s, delta_mat):
+        """
+        Args:
+            s (Numpy array): The state.
+            delta_mat (Numpy array): A 2D array of shape (batch_size, num_actions). Each row of delta_mat  
+            correspond to one state in the batch. Each row has only one non-zero element 
+            which is the TD-error corresponding to the action taken.
+        Returns:
+            The TD update (Array of dictionaries with gradient times TD errors) for the network's weights
+        """
+
+        W0, b0 = self.weights[0]['W'], self.weights[0]['b']
+        W1, b1 = self.weights[1]['W'], self.weights[1]['b']
+        W2, b2 = self.weights[2]['W'], self.weights[2]['b']
+        
+        psi0 = np.dot(s, W0) + b0
+        x0 = np.maximum(psi0, 0)
+        dx0 = (psi0 > 0).astype(float)
+
+        psi1 = np.dot(x0, W1) + b1
+        x1 = np.maximum(psi1, 0)
+        dx1 = (psi1 > 0).astype(float)
+
+        # td_update has the same structure as self.weights, that is an array of dictionaries.
+        # td_update[0]["W"], td_update[0]["b"], td_update[1]["W"], and td_update[1]["b"] have the same shape as 
+        # self.weights[0]["W"], self.weights[0]["b"], self.weights[1]["W"], and self.weights[1]["b"] respectively
+        td_update = [dict() for i in range(len(self.weights))]
+         
+        v = delta_mat
+        td_update[2]['W'] = np.dot(x1.T, v) * 1. / s.shape[0]
+        td_update[2]['b'] = np.sum(v, axis=0, keepdims=True) * 1. / s.shape[0]
+
+        v = np.dot(v, W2.T) * dx1
+        td_update[1]['W'] = np.dot(x0.T, v) * 1. / s.shape[0]
+        td_update[1]['b'] = np.sum(v, axis=0, keepdims=True) * 1. / s.shape[0]
+
+        v = np.dot(v, W1.T) * dx0
+        td_update[0]['W'] = np.dot(s.T, v) * 1. / s.shape[0]
+        td_update[0]['b'] = np.sum(v, axis=0, keepdims=True) * 1. / s.shape[0]
+                
+        return td_update
+    
+    # Work Required: No. You may wish to read the relevant paper for more information on this weight initialization
+    # (Exact solutions to the nonlinear dynamics of learning in deep linear neural networks by Saxe, A et al., 2013)
+    def init_saxe(self, rows, cols):
+        """
+        Args:
+            rows (int): number of input units for layer.
+            cols (int): number of output units for layer.
+        Returns:
+            NumPy Array consisting of weights for the layer based on the initialization in Saxe et al.
+        """
+        tensor = self.rand_generator.normal(0, 1, (rows, cols))
+        if rows < cols:
+            tensor = tensor.T
+        tensor, r = np.linalg.qr(tensor)
+        d = np.diag(r, 0)
+        ph = np.sign(d)
+        tensor *= ph
+
+        if rows < cols:
+            tensor = tensor.T
+        return tensor
+    
+    # Work Required: No.
+    def get_weights(self):
+        """
+        Returns: 
+            A copy of the current weights of this network.
+        """
+        return deepcopy(self.weights)
+    
+    # Work Required: No.
+    def set_weights(self, weights):
+        """
+        Args: 
+            weights (list of dictionaries): Consists of weights that this network will set as its own weights.
+        """
+        self.weights = deepcopy(weights)
+
+class ActionValueNetwork1L:
     # Work Required: Yes. Fill in the layer_sizes member variable (~1 Line).
     def __init__(self, network_config):
         self.state_dim = network_config.get("state_dim")
@@ -579,7 +702,7 @@ def run_experiment(num_episodes):
     agent_info = {
              'network_config': {
                  'state_dim': 8,
-                 'num_hidden_units': 256,
+                 'num_hidden_units': 128,
                  'num_hidden_layers': 1,
                  'num_actions': 4
              },
